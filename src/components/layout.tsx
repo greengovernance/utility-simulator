c920d62193b51c46c80e3250a1c3aeed7acb8238
Eric McKay <ericmckay4@gmail.com>
2019-07-12 09:58:39 -0700

    add boilerplate

---
+++ src/components/layout.tsx
@@ -0,0 +1,43 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './header'
+import './layout.css'
+import '../styles/main.scss'
+
+const Layout: React.SFC<{ children?: React.ReactNode }> = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+        </div>
+      </>
+    )}
+  />
+)
+
+export default Layout
