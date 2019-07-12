d7895937f5b00c274a88bb4813ea6fbcb0747ac7
Eric McKay <ericmckay4@gmail.com>
2019-07-11 17:44:43 -0700

    add pre-alpha warning

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,14 +13,32 @@ import GreenGovernanceLogo from '../images/green-governance-logo-transparent.svg
 import { DiGithubAlt } from 'react-icons/di'
 import Navbar from 'reactstrap/lib/Navbar'
 import NavbarBrand from 'reactstrap/lib/NavbarBrand'
+import Alert from 'reactstrap/lib/Alert'
 
-const Layout: React.SFC<{ children?: React.ReactNode }> = ({ children }) => (
+const Layout: React.SFC<{
+  showAlphaWarning?: boolean
+  children?: React.ReactNode
+}> = ({ children, showAlphaWarning }) => (
   <div className="layout d-flex min-vh-100 flex-column">
-    <Navbar light color="light">
-      <Container>
-        <NavbarBrand>Utility Simulator</NavbarBrand>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar light color="light">
+        <Container>
+          <NavbarBrand>Utility Simulator</NavbarBrand>
+        </Container>
+      </Navbar>
+      {showAlphaWarning && (
+        <Alert color="danger">
+          <Container>
+            <b>Warning!</b> This software is in a pre-alpha state. Please report
+            any issues or learn how to contribute on{' '}
+            <a href="https://github.com/greengovernance/utility-simulator">
+              GitHub
+            </a>
+            .
+          </Container>
+        </Alert>
+      )}
+    </div>
     <Container className="flex-grow-1 mt-3">
       <main>{children}</main>
     </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import SEO from '../components/seo'
 import GameContainer from '../components/game/game-container'
 
 const IndexPage = () => (
-  <Layout>
+  <Layout showAlphaWarning>
     <SEO title="Home" />
     <GameContainer />
   </Layout>
